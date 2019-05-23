767abbe5a9de5f91cfbcef0f2fb926f43a3e3856
andrewpsuedonym <j.m.walker@live.co.uk>
2019-05-23 13:50:57 +0100

    made bandwitdh graph of data

--- babel/results/parse.py
+++ babel/results/parse.py
@@ -11,11 +11,11 @@ def parse_file(f):
     i = 0
     while i < len(data):
         if data[i].split()[0] == 'Copy':
-            res[0] += [float(data[i].split()[4])]
-            res[1] += [float(data[i+1].split()[4])]
-            res[2] += [float(data[i+2].split()[4])]
-            res[3] += [float(data[i+3].split()[4])]
-            res[4] += [float(data[i+4].split()[4])]
+            res[0] += [float(data[i].split()[1])]
+            res[1] += [float(data[i+1].split()[1])]
+            res[2] += [float(data[i+2].split()[1])]
+            res[3] += [float(data[i+3].split()[1])]
+            res[4] += [float(data[i+4].split()[1])]
         i+=1
 
     return res
@@ -29,15 +29,23 @@ def get_speedup(res):
         res[i] = t1/res[i]
         i += 1
 
+def get_Gb(results):
+    i = 0
+    while i < len(results):
+        res[i] = res[i] / 1000
+        i += 1
+
 if __name__ == '__main__':
     label1 = (sys.argv[1].split('_')[0])
     label2 = (sys.argv[2].split('_')[0])
     res1 = parse_file(sys.argv[1])
     res2 = parse_file(sys.argv[2])
+    print(res1[0][0])
     for res in res1:
-        get_speedup(res)
+        get_Gb(res)
     for res in res2:
-        get_speedup(res)
+        get_Gb(res)
+    print(res1[0][0])
 
 
     functions = ['Copy', 'Mul', 'Add', 'Triad', 'Dot']
@@ -48,7 +56,7 @@ if __name__ == '__main__':
         fig, ax = plt.subplots()
         ax.plot(x_axis, res1[i], label=label1, marker='.')
         ax.plot( x_axis, res2[i], label=label2, marker='.')
-        ax.set(xlabel='Number of threads', ylabel='Speedup (t1/tn)',
+        ax.set(xlabel='Number of threads', ylabel='Bandwidth GBytes/sec',
                title=functions[i])
         ax.set_xticklabels(x_axis)
         ax.legend()
