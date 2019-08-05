53221893215104f64a49f98e86a21e545112ab73
andrewpsuedonym <j.m.walker@live.co.uk>
2019-08-05 18:49:51 +0100

    data collection

--- sparse/sparse/src/main.rs
+++ sparse/sparse/src/main.rs
@@ -234,6 +234,7 @@ fn main() {
     }
     let end_time = sparse_time.elapsed();
 
+    println!("Vector sizes\n result: {} matrix: {} vector: {} col_index: {}", result.len(), matrix.len(), vector.len(), col_index.len());
     // Verification test
     let reference_sum = 0.5 * nent as f64 * f64::from(iterations + 1) * f64::from(iterations + 2);
 
